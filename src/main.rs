b79bffa55e2c0b6ae0310f2faf1f306e4bb1675c
X1Vi <gundeep477@gmail.com>
2025-12-03 17:34:57 +0530

    increased sleep timer

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ use crate::dbn::dbn_local::start_server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    start_server("127.0.0.1:8080", "CLX5_mbo.dbn".to_string(), 0).await
+    start_server("127.0.0.1:8080", "CLX5_mbo.dbn".to_string(), 400).await
 }
 
